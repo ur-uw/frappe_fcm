22736a666d9213ce701411b449aba4c56343c829
Mohammed Fadhil <mohammedawni@uomustansiriyah.edu.iq>
2024-04-16 02:19:29 +0300

    feat: :sparkles: migrate to the new admin sdk

--- fcm_notification/send_notification.py
+++ fcm_notification/send_notification.py
@@ -1,8 +1,20 @@
+import re
+
+import firebase_admin
 import frappe
 import requests
-import json
+from firebase_admin import credentials, messaging
 from frappe import enqueue
-import re
+
+
+def initialize_firebase():
+    try:
+        admin = firebase_admin.get_app()
+    except ValueError:
+        cred = credentials.Certificate(
+            frappe.get_site_path() + "/private/files/credentials.json"
+        )
+        firebase_admin.initialize_app(credential=cred)
 
 
 def user_id(doc):
@@ -14,16 +26,18 @@ def user_id(doc):
 
 
 @frappe.whitelist()
-def send_notification(doc, event):
-    device_ids = user_id(doc)
-    for device_id in device_ids:
-        enqueue(
-            process_notification,
-            queue="default",
-            now=False,
-            device_id=device_id,
-            notification=doc,
-        )
+def send_notification(doc, event=None):
+    if event != None:
+        device_ids = user_id(doc)
+        for device_id in device_ids:
+            enqueue(
+                process_notification,
+                queue="default",
+                now=False,
+                device_id=device_id,
+                notification=doc,
+                job_id=device_id.device_id,
+            )
 
 
 def convert_message(message):
@@ -34,32 +48,49 @@ def convert_message(message):
 
 
 def process_notification(device_id, notification):
+    initialize_firebase()
     message = notification.email_content
     title = notification.subject
     if message:
         message = convert_message(message)
     if title:
         title = convert_message(title)
-
-    url = "https://fcm.googleapis.com/fcm/send"
-    body = {
-        "to": device_id.device_id,
-        "notification": {"body": message, "title": title},
-        "data": {
-            "doctype": notification.document_type,
-            "docname": notification.document_name,
-        },
+    data = {
+        "doctype": notification.document_type,
+        "docname": notification.document_name,
+        "title": title,
+        "message": message,
     }
 
-    server_key = frappe.db.get_single_value("FCM Notification Settings", "server_key")
-    auth = f"Bearer {server_key}"
-    req = requests.post(
-        url=url,
-        data=json.dumps(body),
-        headers={
-            "Authorization": auth,
-            "Content-Type": "application/json",
-            "Accept": "application/json",
-        },
-    )
-    frappe.log_error(req.text)
+    try:
+        messaging.send(
+            message=messaging.Message(
+                data=data,
+                token=device_id.device_id,
+                android=messaging.AndroidConfig(
+                    priority="normal",
+                    notification=messaging.AndroidNotification(
+                        title=title,
+                        body=message,
+                        channel_id="high_importance_channel",
+                    ),
+                    fcm_options=messaging.AndroidFCMOptions(
+                        analytics_label="assign_agent_to_order",
+                    ),
+                ),
+                apns=messaging.APNSConfig(
+                    payload=messaging.APNSPayload(
+                        aps=messaging.Aps(
+                            alert=messaging.ApsAlert(title=title, body=message),
+                            sound="default",
+                        ),
+                    )
+                ),
+                fcm_options=messaging.FCMOptions(
+                    analytics_label="assign_agent_to_order",
+                ),
+            ),
+        )
+    except Exception as e:
+        frappe.error_log(f"Error sending notification: {e}")
+        pass
