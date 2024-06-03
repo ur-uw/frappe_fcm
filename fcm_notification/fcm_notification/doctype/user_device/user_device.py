be5e67a6249282460224ed99984d9c864d54ebe6
Mohammed Fadhil <mohammedawni@uomustansiriyah.edu.iq>
2024-06-03 23:18:20 +0300

    feat: :sparkles: add end point to handle device info

--- fcm_notification/fcm_notification/doctype/user_device/user_device.py
+++ fcm_notification/fcm_notification/doctype/user_device/user_device.py
@@ -1,8 +1,28 @@
 # Copyright (c) 2022, Raheeb and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
+
 class UserDevice(Document):
-	pass
+    pass
+
+
+@frappe.whitelist()
+def handle_user_device(device_data):
+    device_id = device_data.get("device_id")
+    if not device_id:
+        frappe.throw(frappe._("Device ID is required"))
+    if frappe.db.exists("User Device", {"device_id": device_id}):
+        # Update the existing record
+        user_device = frappe.get_doc("User Device", {"device_id": device_id})
+        user_device.update(device_data)
+        user_device.save(ignore_permissions=True)
+        frappe.db.commit()
+        return user_device
+    user_device = frappe.new_doc("User Device")
+    user_device.update(device_data)
+    user_device.save(ignore_permissions=True)
+    frappe.db.commit()
+    return user_device
